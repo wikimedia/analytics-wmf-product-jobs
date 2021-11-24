5c1483b5b5437e462423e0cad9755b572361bf3a
Mikhail Popov <mpopov@wikimedia.org>
2021-11-24 10:12:04 -0500

    movement_metrics: Cleanup notebooks dir
    Removes notebooks created by nbconvert on the previous
    execution of the script.
    
    Bug: T296397
    Change-Id: Ib4f59225cdcad7f035c9696c83c3620be01b2db0

--- movement_metrics/main.sh
+++ movement_metrics/main.sh
@@ -2,6 +2,10 @@
 
 notebooks_dir="/srv/product_analytics/jobs/movement_metrics/notebooks"
 
+# Clean up nbconvert notebooks from *previous* run:
+rm $notebooks_dir/*.nbconvert.ipynb
+
+# Activate Anaconda-WMF base environment:
 source /usr/lib/anaconda-wmf/bin/activate
 
 for notebook in $notebooks_dir/*.ipynb
